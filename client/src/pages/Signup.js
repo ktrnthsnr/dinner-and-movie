3622cc8fc252d6ca746298a615984550b8803b94
unknown <asakthisner@hotmail.com>
2020-08-14 17:49:03 -0700

    added fonts to title

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,59 +1,43 @@
 import React, { useState } from 'react';
 
-// new -- importing mutations, hooks to connect signup and login mutations from utils
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 
-// new -- adding JSON Web Token (JWT) authentication from the utils auth.js
+
 import Auth from '../utils/auth';
 
-// capture form field data and store in a state using useState() hook
+
 const Signup = () => {
-  // declare formState
+
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
 
-  // new -- add the mutation
+
   const [addUser, { error }] = useMutation(ADD_USER);
 
-  // update state based on form input changes
+
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-      // console.log(formState);
-      // console.log(name);
 
     setFormState({ ...formState, [name]: value, });
   };
 
-// new -- added function detail
-  // submit form (notice the async!)
   const handleFormSubmit = async event => {
     event.preventDefault();
 
-    // console.log(formState);
 
-
-    // use try/catch instead of promises to handle errors
     try {
-      // execute addUser mutation and pass in variable data from the form state object as variables for the mutation function
+
       const { data } = await addUser({
         variables: { ...formState }
       });
-      // new -- added JSON Web Token (JWT)
       Auth.login(data.addUser.token);
 
-      // console.log(data);
 
     } catch (e) {
       console.error(e);
     }
   };
-
-
-  // // remove -- submit form - placeholder
-      // const handleFormSubmit = async (event) => {
-      //   event.preventDefault();
-      // };
 
 
   return (
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,27 +1,20 @@
 import React from 'react';
 
-// new -- import from the new components
 import ReactionList from '../components/ReactionList';
 import ReactionForm from '../components/ReactionForm';
 
-// new -- import the React hook, to access ID from the URL
+
 import { useParams } from 'react-router-dom';
 
-// new -- useQuery hook to import from utils/queries
+
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 
-// new -- adding JSON Web Token (JWT) authentication from the utils auth.js, for adding Reaction component
+
 import Auth from '../utils/auth';
 
 const SingleThought = props => {
 
-  // new / prev -- initial  check to access ID from the URL from console.log
-    // const { id: thoughtId } = useParams();
-    // console.log(thoughtId);
-
-  // new -- adding thought object from utils/queries.js
-    // ( id property on the variables object will become the $id param in the GraphQL query)
     const { id: thoughtId } = useParams();
     const { loading, data } = useQuery(QUERY_THOUGHT, {
       variables: { id: thoughtId }
@@ -31,7 +24,6 @@ const SingleThought = props => {
       return <div>Loading...</div>;
     }
 
-    // new -- after adding ReactionList component & thought object above
     return (
       <div>
        <div>
@@ -55,41 +47,5 @@ const SingleThought = props => {
     );
   };
 
-
-  // // new -- after adding thought object, updated the JSX
-      //   return (
-      //     <div>
-      //       <div className="card mb-3">
-      //         <p className="card-header">
-      //           <span style={{ fontWeight: 700 }} className="text-light">
-      //             {thought.username}
-      //           </span>{' '}
-      //           thought on {thought.createdAt}
-      //         </p>
-      //         <div className="card-body">
-      //           <p>{thought.thoughtText}</p>
-      //         </div>
-      //       </div>
-      //   </div>
-      //   );
-      // };
-
-// // before
-    //   return (
-    //     <div>
-    //       <div className="card mb-3">
-    //         <p className="card-header">
-    //           <span style={{ fontWeight: 700 }} className="text-light">
-    //             Username
-    //           </span>{' '}
-    //           thought on createdAt
-    //         </p>
-    //         <div className="card-body">
-    //           <p>Thought Text</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   );
-    // };
 
 export default SingleThought;
