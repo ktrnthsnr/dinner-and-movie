e82967b2d6edd63f2931cb8994a6fa23d7b71160
katguerin <katguerin55@gmail.com>
2020-08-18 20:27:24 -0700

    latest push in class!

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/landing.css";
+import Carousel from 'react-bootstrap/Carousel';
+
 // import img1 from "../images/curtain.jpg";
 // import img2 from "../images/catcouch.jpg";
 import img3 from "../images/hobbit.jpg";
@@ -28,23 +30,23 @@ const carouselSlides = [
 const Landing = (props) => {
   const [slides, setSlides] = React.useState(carouselSlides);
   return (
-    <div className="carousel slide" >
-        <div className="carousel-inner min-80-vh">
+    <div class="carousel slide" >
+        <div class="carousel-inner min-80-vh">
       {slides.map((slide) => (
         <div
-          className="carousel-item fullscreen-carousel "
+          class="carousel-item fullscreen-carousel "
           style={{ backgroundImage: `url('${process.env.PUBLIC_URL + slide.img}')` }}
         >
-          <div className="d-flex h-100 align-items-center justify-content-center carousel-caption">
-            <div className="container">
-              <div className="row align-items-center justify-content-center">
-                <h2 className="display-4 mb-2">{slide.title}</h2>
+          <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
+            <div class="container">
+              <div class="row align-items-center justify-content-center">
+                <h2 class="display-4 mb-2">{slide.title}</h2>
               </div>
-              <div className="row align-items-center justify-content-center">
+              <div class="row align-items-center justify-content-center">
                 <h3>{slide.subtitle}</h3>
               </div>
-              <div className=" mt-4 row align-items-center justify-content-center">
-                <a className="btn btn-primary" href={slide.btnUrl}>
+              <div class=" mt-4 row align-items-center justify-content-center">
+                <a class="btn btn-primary" href={slide.btnUrl}>
                   {slide.btnText}
                 </a>
               </div>
